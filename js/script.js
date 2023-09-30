128681fd352d7de6714f861b3a2f799680b7462a
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2023-09-30 22:44:58 +0200

    fixed all frontend stuff

--- js/script.js
+++ js/script.js
@@ -1,68 +1,80 @@
-const axios = require('axios');
-
-// Define the anime title you want to search for
-const animeTitle = 'The Rising of the Shield Hero Season 3';
-
-// Make a search request to the AniList API
-axios.get(`https://graphql.anilist.co`, {
-  params: {
-    query: `
-      query ($title: String) {
-        Media (search: $title, type: ANIME) {
-          title {
-            romaji
-          }
-          nextAiringEpisode {
-            airingAt
-          }
-        }
-      }
-    `,
-    variables: { title: animeTitle },
-  },
-})
-.then((response) => {
-  const anime = response.data.data.Media[0];
-  if (anime) {
-    const airingDate = new Date(anime.nextAiringEpisode.airingAt * 1000); // Convert Unix timestamp to JavaScript date
-    console.log(`Anime: ${anime.title.romaji}`);
-    console.log(`Next Airing Date: ${airingDate.toISOString()}`);
-  } else {
-    console.log('Anime not found.');
-  }
-})
-.catch((error) => {
-  console.error(error);
-});
-
-
-
-
-
-
-
-
-
-
-
-// Function to update the countdown timer
-function updateCountdown() {
-    const targetDate = new Date('2023-10-05T00:00:00'); // Set your target date and time
-    const currentDate = new Date();
-    const timeDifference = targetDate - currentDate;
-
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-
-    document.getElementById('countdown-timer').textContent = `${days}d ${hours}h ${minutes}m`;
+window.onload = async function () {
+  setInterval(async function () {
+    await updateCountdown();
+  }, 60000);
+  // Initial call to set the countdown
+  updateCountdown();
 }
 
-// Update the countdown every second
-setInterval(updateCountdown, 1000);
 
-// Initial call to set the countdown
-updateCountdown();
+async function updateCountdown() {
+  const countdownTimer = document.getElementById('countdown-timer');
+
+  const apiUrl = 'https://graphql.anilist.co';
+  const animeTitle = 'The Rising of the Shield Hero Season 3';
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+        query ($title: String) {
+          Media (search: $title, type: ANIME) {
+            title {
+              romaji
+            }
+            nextAiringEpisode {
+              airingAt
+            }
+          }
+        }
+      `,
+      variables: { title: animeTitle },
+    }),
+  };
+
+  let _response;
+  try {
+    _response = await fetch(apiUrl, requestOptions);
+  } catch (error) {
+    throw new Error(`Network response was not ok: ${error}`);
+  }
+  const response = _response;
+
+  if (!response.ok) {
+    throw new Error(`Network response was not ok.`);
+  }
+
+  const responseJson = await response.json();
+  const anime = responseJson.data.Media;
+  if (!anime) {
+    throw new Error(`Anime not found.`);
+  }
+
+  const airingDate = formatCountdown(anime.nextAiringEpisode.airingAt);
+  countdownTimer.textContent = airingDate;
+}
 
 
+function formatCountdown(unixTimestamp) {
+  // Create a Date object using the Unix timestamp (multiply by 1000 to convert to milliseconds)
+  const targetDate = new Date(unixTimestamp * 1000);
 
+  // Get the current date and time
+  const currentDate = new Date();
+
+  // Calculate the time difference in milliseconds
+  const timeDifference = targetDate - currentDate;
+
+  // Calculate days, hours, and minutes
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+
+  // Format the result
+  const formattedCountdown = `Ep 1: ${days}d ${hours}h ${minutes}m`;
+
+  return formattedCountdown;
+}
